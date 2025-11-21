91addce126bd35536974ce410567206eb0142b7d
Dylan Marley <dylanjmarley01@gmail.com>
2025-11-21 16:38:54 +0000

    Button to Top added

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log("Hello, World!");
+
+const btnToTop = document.getElementById("btn-to-top");
+
+btnToTop.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
